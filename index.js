b938e2a5435adfffd3954c1ca0f7521427b076f1
Nikita <terencekity@gmail.com>
2023-09-11 12:35:35 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 15.5
+let num2 = 4
+let multiply = num1*num2
+let random = Math.floor(Math.random()+1)
+let num3 = 20
+let num4 = 8
+let mod = num3%num4
+let max = Math.max(num1,num2,num3,num4)
